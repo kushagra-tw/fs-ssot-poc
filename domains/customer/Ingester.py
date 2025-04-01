6d3001d6c1566a28ee6005c19728b7b78c036967
Kushagra <kushagra.singh@thoughtworks.com>
2025-04-01 16:32:37 +0530

    add take nces id and nces data method

--- domains/customer/Ingester.py
+++ domains/customer/Ingester.py
@@ -1,6 +1,6 @@
 
 from Reader import read_data
-
+import pandas as pd
 def populate_nces_data(focus_data, sf_data):
     # for r in focusList[]:
 #         ncesID = ''
@@ -35,6 +35,29 @@ def process_data():
     sf_data = read_data( sf_file)
     validated_focus_data = validate_focus_data(focus_data)
     populate_nces_data(validated_focus_data, sf_data)
+
+def takeNCESIDFromSF(merged_df,focus_only_df):
+    """
+    This function takes dataframe as input and returns all the focus colum and only nces id column from the salesforce
+    :param merged_df: combined dataframe for focus and salesforce customer
+    :param focus_only_df: focus only df
+    :return: dataframe
+    """
+    focus_columns = focus_only_df.columns.to_list()
+    columns_to_be_included = focus_columns + ['NCES_ID__C']
+    merged_df = merged_df[columns_to_be_included]
+    return merged_df
+
+def takeNCESData(merged_df,nces_df,left_key_col,right_key_col):
+    """
+    Use to join dataframe which has both focus and salesforce data with nces data using nces id.
+    :param merged_df: dataframe containing focus data and nces id column from the salesforce data
+    :param nces_df: dataframe containing only nces record
+    :param left_key_col: col for left dataframe which will be used in join
+    :param right_key_col: col for right dataframe which will be used in join
+    :return: merged dataframe which has focus and nces data.
+    """
+    return pd.merge(left=merged_df, right=nces_df, how='left', left_on=f'{left_key_col}', right_on=f'{right_key_col}')
 
 def main():
     process_data()
